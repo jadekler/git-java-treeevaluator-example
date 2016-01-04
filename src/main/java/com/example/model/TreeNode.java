db01253c0e2f2df876ea768e590b28234639832f
Jean de Klerk <jadekler@gmail.com>
2016-01-04 05:44:36 -0700

    Fleshed out tree builder

--- src/main/java/com/example/model/TreeNode.java
+++ src/main/java/com/example/model/TreeNode.java
@@ -1,4 +1,68 @@
 package com.example.model;
 
-public class TreeNode {
+import java.util.*;
+
+import static java.util.Collections.emptyList;
+
+public class TreeNode<T> {
+    private String description;
+    private List<TreeNode<T>> children = emptyList();
+    private Optional<LogicalOperator> logicalOperator = Optional.empty();
+    private Optional<T> criteria = Optional.empty();
+
+    public TreeNode(String description, List<TreeNode<T>> children, Optional<LogicalOperator> logicalOperator) {
+        this.description = description;
+        this.children = children;
+    }
+
+    public TreeNode(String description, Optional<T> criteria) {
+        this.description = description;
+        this.criteria = criteria;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<TreeNode<T>> getChildren() {
+        return children;
+    }
+
+    public Optional<T> getCriteria() {
+        return criteria;
+    }
+
+    @Override
+    public String toString() {
+        return "TreeNode{" +
+            "description='" + description + '\'' +
+            ", children=" + children +
+            ", criteria=" + criteria +
+            '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        TreeNode<?> treeNode = (TreeNode<?>) o;
+
+        if (description != null ? !description.equals(treeNode.description) : treeNode.description != null)
+            return false;
+        if (children != null ? !children.equals(treeNode.children) : treeNode.children != null)
+            return false;
+        return criteria != null ? criteria.equals(treeNode.criteria) : treeNode.criteria == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = description != null ? description.hashCode() : 0;
+        result = 31 * result + (children != null ? children.hashCode() : 0);
+        result = 31 * result + (criteria != null ? criteria.hashCode() : 0);
+        return result;
+    }
 }
--- src/main/java/com/example/service/TreeBuilder.java
+++ src/main/java/com/example/service/TreeBuilder.java
@@ -3,16 +3,45 @@ package com.example.service;
 import com.example.model.*;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
+
+import static com.example.model.LogicalOperator.AND;
+import static com.example.model.LogicalOperator.OR;
+import static java.util.stream.Collectors.toList;
 
 @Service
 public class TreeBuilder {
-    public TreeNode build(
+    public TreeNode<Grade> build(
         School school,
         List<Subject> allSubjects,
         List<Level> allLevels,
         List<Grade> allGrades
     ) {
-        return null;
+        List<String> subjectIds = school.getSubjectIds();
+
+        List<TreeNode<Grade>> schoolChildren = allSubjects.stream()
+            .filter(subjectIds::contains)
+            .map(subject -> {
+                List<String> levelIds = subject.getLevelIds();
+
+                List<TreeNode<Grade>> subjectChildren = allLevels.stream()
+                    .filter(levelIds::contains)
+                    .map(level -> {
+                        List<String> gradeIds = level.getGradeIds();
+
+                        List<TreeNode<Grade>> levelChildren = allGrades.stream()
+                            .filter(gradeIds::contains)
+                            .map(grade -> new TreeNode<>(grade.getId(), Optional.of(grade)))
+                            .collect(toList());
+
+                        return new TreeNode<>(level.getId(), levelChildren, Optional.of(AND));
+                    })
+                    .collect(toList());
+
+                return new TreeNode<>(subject.getId(), subjectChildren, Optional.of(OR));
+            })
+            .collect(toList());
+
+        return new TreeNode<>(school.getId(), schoolChildren, Optional.of(AND));
     }
 }
