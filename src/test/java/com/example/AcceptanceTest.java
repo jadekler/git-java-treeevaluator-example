b8fcbe20cbd5b5ea4e9b2d09df3f1e7241d32e27
Jean de Klerk <jadekler@gmail.com>
2016-01-07 18:19:01 -0700

    Acceptance tests

---
+++ src/test/java/com/example/AcceptanceTest.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import com.example.config.DemoApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.*;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static com.jayway.restassured.RestAssured.get;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DemoApplication.class)
+@WebIntegrationTest("server.port:0")
+public class AcceptanceTest {
+    @Value("${local.server.port}")
+    private int port;
+
+	@Test
+	public void algorithm() {
+        get("http://127.0.0.1:" + port + "/schools?commaSeparatedGrades=Pass")
+            .then().assertThat().body("get(0).id", equalTo("school id 1"))
+            .assertThat().body("size()", equalTo(1));
+
+        get("http://127.0.0.1:" + port + "/schools?commaSeparatedGrades=Pass,Fail")
+            .then().assertThat().body("get(0).id", equalTo("school id 1"))
+            .assertThat().body("get(1).id", equalTo("school id 2"))
+            .assertThat().body("size()", equalTo(2));
+	}
+}
